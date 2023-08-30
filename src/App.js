8d39f5fb2968fd24652f9ff6fd2e5b2528968592
Mayank Sandilya <78838306+beingkamikaze@users.noreply.github.com>
2023-08-31 03:01:48 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+
+import "./styles/App.scss";
+import "./styles/header.scss";
+import "./styles/home.scss";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <main>
+            <h1>TechyStar</h1>
+            <p>Solution to all your problems</p>
+        </main>
+    </div>
+  )
+};
+
+export default Home;
